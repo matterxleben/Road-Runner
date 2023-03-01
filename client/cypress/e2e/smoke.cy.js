45cf06caf137ddb59933850061bba1e3ca79cc1b
John Kach <johnkach@johns-mbp-2.home>
2023-03-01 16:20:09 -0500

    Minor changes to AddRun sql query

--- client/cypress/e2e/smoke.cy.js
+++ client/cypress/e2e/smoke.cy.js
@@ -2,5 +2,9 @@ describe('Smoke Test', () => {
   it('can view the home page', () => {
     cy.visit('/');
     cy.contains('Join Event');
+    cy.contains('Add Event');
+    cy.contains('Add Run');
+    cy.contains('Add Friend');
+    
   });
 });
