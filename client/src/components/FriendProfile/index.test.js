d3cacf576528a47106edc5a62ee8cfc492a5ffb7
John Kach <johnkach@v1040-wn-rt-b-116-173.campus-dynamic.uwaterloo.ca>
2023-03-28 13:08:39 -0400

    Added JEST tests to UserDetails and FriendProfile page

---
+++ client/src/components/FriendProfile/index.test.js
@@ -0,0 +1,29 @@
+﻿
+//tests by John
+
+import { render, fireEvent, screen, getByRole, getByText, queryByText, getByDisplayValue } from "@testing-library/react";
+import index from "./index";
+import React from 'react';
+
+describe('index', () => {
+
+
+    //if Profile headers are there
+    it("should show profile headers", () => {
+        render(<index />)
+        expect(screen.queryByText(`City`))
+        expect(screen.queryByText(`Height`))
+        expect(screen.queryByText(`Weight`))
+
+    })
+
+
+    //if Run Log is there
+    it("should show run log", () => {
+        render(<index />)
+        expect(screen.queryByText(`Runner's Log`))
+
+    })
+
+
+})
